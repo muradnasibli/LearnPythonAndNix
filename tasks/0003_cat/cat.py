8cc35795f2c127771c4d0db18ea1a4b2792f1ba5
muradnasibli <muradin@code.edu.az>
2020-09-22 19:25:28 +0400

    0003_cat task finished

--- tasks/0003_cat/cat.py
+++ tasks/0003_cat/cat.py
@@ -1,1 +1,11 @@
 # Write your code here.
+import os, sys
+
+n = len(sys.argv)
+
+for x in range(1,n):
+    if os.path.exists(sys.argv[x]):
+        f = open(sys.argv[x], "r")
+        print("contents of " + sys.argv[x],f.read())
+    else:
+        print("This file doesn't exists! :" + sys.argv[x])
