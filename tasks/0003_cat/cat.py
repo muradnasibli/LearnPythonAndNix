2b2dace5dd0cda10fa612c7236b74d2311fc8a86
muradnasibli <muradin@code.edu.az>
2020-09-23 13:07:42 +0400

    0003_cat task mistake fixed

--- tasks/0003_cat/cat.py
+++ tasks/0003_cat/cat.py
@@ -1,11 +1,12 @@
 # Write your code here.
 import os, sys
 
+#Lenght of arguments
 n = len(sys.argv)
 
 for x in range(1,n):
     if os.path.exists(sys.argv[x]):
-        f = open(sys.argv[x], "r")
-        print("contents of " + sys.argv[x],f.read())
+       with open(sys.argv[x]) as f:
+           print("Content of :" + sys.argv[x] + "\n", f.read())
     else:
         print("This file doesn't exists! :" + sys.argv[x])
